9ce13c1d2a1d5e88425d176a63523bbc31532449
the-doyle <benjaminmarkdoyle@gmail.com>
2021-03-16 11:34:34 -0600

    initialized group and appointment models

---
+++ Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_1.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [Required]
+        public int AppointmentID { get; set; } //appointment id
+
+        [Required]
+        public DateTime Timeslot { get; set; } //timeslot for temple visit 
+
+        [Required]
+        public bool Available { get; set; } //whether the appointment is available or not
+
+    }
+
+}
---
+++ Models/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_1.Models
+{
+    public class Group : Appointment //group class inherits from appointment class 
+    {
+        [Key]
+        [Required]
+        public int GroupID { get; set; } //group id
+
+        [Required]
+        public string Name { get; set; } //group name
+
+        [Required]
+        public int Size { get; set; } //group size 
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; } //email address 
+
+        [Phone]
+        public int Phone { get; set; } //phone number (optional)
+
+        
+    }
+}
