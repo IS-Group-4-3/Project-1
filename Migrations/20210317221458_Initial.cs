a1153294d6444e6787a8408eed3698874ec79680
the-doyle <benjaminmarkdoyle@gmail.com>
2021-03-17 16:23:14 -0600

    * AppointmentDb.sqlite: * AppointmentDb.sqlite-shm: * AppointmentDb.sqlite-wal: * 20210317221050_Initial.cs: * 20210317221458_Initial.cs: * 20210317221050_Initial.Designer.cs: * 20210317221458_Initial.Designer.cs:
    * Appointment.cs: change db

---
+++ Migrations/20210317221458_Initial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project_1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Day = table.Column<string>(type: "TEXT", nullable: false),
+                    Hour = table.Column<int>(type: "INTEGER", nullable: false),
+                    AmPm = table.Column<string>(type: "TEXT", nullable: false),
+                    Available = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Size = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    AppointmentID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Phone = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
