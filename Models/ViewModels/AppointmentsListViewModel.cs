b8d283a0bfd394b8dbb9375b6dbabb84a24598bf
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-03-17 14:24:11 -0600

    made AppointmentsListViewModel

---
+++ Models/ViewModels/AppointmentsListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_1.Models.ViewModels
+{
+    public class AppointmentsListViewModel
+    {
+        public IEnumerable<Appointment> appointments { get; set; }
+        //public PagingInfo PagingInfo { get; set; }
+
+        //Added a currentcategory variable
+        //public string CurrentDay { get; set; }
+
+    }
+}
